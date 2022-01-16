c631f91178d26f38790a241b2ce32e8123ab2e7d
Alec Deitloff <alec.deitloff@gmail.com>
2022-01-16 13:19:05 -0800

    Replace convict with AJV

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,153 +1,85 @@
-import convict from "convict";
+import Ajv, { JSONSchemaType } from "ajv";
 import { cosmiconfigSync } from "cosmiconfig";
 
 import type {
   AssetDefinition,
-  AssetDefinitions,
   BucketDefinition,
   CloudfrontDefinition,
   Config,
-  Confirmation,
 } from "../types";
 
 import { COSMICONFIG_MODULE_NAME } from "./constants";
 
-const CONVICT_FORMAT_NONEMPTY_STRING = "nonempty-string";
-
-convict.addFormat({
-  coerce: function (val: unknown): string {
-    return String(val);
-  },
-  name: CONVICT_FORMAT_NONEMPTY_STRING,
-  validate: function (val: unknown): asserts val is string {
-    if (typeof val !== "string") {
-      throw new Error("Must be a string");
-    }
-
-    if (!val.length) {
-      throw new Error("String must not be empty");
-    }
-  },
-});
-
-function buildConvictFormat<T>(
-  schema: convict.Schema<T>
-): (val: unknown) => asserts val is T {
-  const validator = convict<T>(schema);
-
-  return (val) => {
-    if (typeof val !== "object" || !val) {
-      throw new Error("Value must be an object.");
-    }
-
-    validator.load(val).validate();
-  };
-}
-
-function assertIsConfirmation(val: unknown): asserts val is Confirmation {
-  switch (typeof val) {
-    case "string":
-    case "function": {
-      return;
-    }
-    default: {
-      throw new Error("Value is not a recognized Confirmation value");
-    }
-  }
-}
-
-const ASSET_DEFINITION_VALIDATOR = convict<AssetDefinition>({
-  contentType: {
-    default: undefined,
-    format: String,
-  },
-});
-
-const CONFIG_VALIDATOR = convict<Config>({
-  assets: {
-    default: {},
-    format: (val: unknown): asserts val is AssetDefinitions | undefined => {
-      if (typeof val === "undefined") {
-        return;
-      }
-
-      if (typeof val !== "object" || !val) {
-        throw new Error("Value, if specified, must be an object");
-      }
-
-      const fields = Object.entries(val);
-      for (const [field, definition] of fields) {
-        switch (typeof definition) {
-          case "undefined":
-          case "boolean": {
-            continue;
-          }
-          case "object": {
-            if (!definition) {
-              throw new Error(`Asset definition '${field}' cannot be null`);
-            }
-
-            if (Array.isArray(definition)) {
-              throw new Error(`Asset definition '${field}' cannot be an array`);
-            }
-
-            ASSET_DEFINITION_VALIDATOR.load(definition).validate();
-            continue;
-          }
-          default: {
-            throw new Error(
-              `Asset definition '${field}' is invalid type: ${typeof definition}`
-            );
-          }
-        }
-      }
+const ASSET_DEFINITION_SCHEMA: JSONSchemaType<AssetDefinition> = {
+  additionalProperties: false,
+  properties: {
+    contentType: {
+      nullable: true,
+      type: "string",
     },
   },
-  bucket: {
-    default: null,
-    format: buildConvictFormat<BucketDefinition>({
-      name: {
-        default: null,
-        format: CONVICT_FORMAT_NONEMPTY_STRING,
-      },
-      region: {
-        default: null,
-        format: CONVICT_FORMAT_NONEMPTY_STRING,
-      },
-    }),
-  },
-  buildDir: {
-    default: null,
-    format: CONVICT_FORMAT_NONEMPTY_STRING,
-  },
-  cloudfront: {
-    default: undefined,
-    format: buildConvictFormat<CloudfrontDefinition>({
-      id: {
-        default: null,
-        format: CONVICT_FORMAT_NONEMPTY_STRING,
-      },
-      region: {
-        default: null,
-        format: CONVICT_FORMAT_NONEMPTY_STRING,
-      },
-    }),
-  },
-  confirmation: {
-    default: undefined,
-    format: (val: unknown) => {
-      if (typeof val === "undefined") {
-        return;
-      }
+  type: "object",
+};
 
-      if (Array.isArray(val)) {
-        val.forEach(assertIsConfirmation);
-      } else {
-        assertIsConfirmation(val);
-      }
+const BUCKET_DEFINITION_SCHEMA: JSONSchemaType<BucketDefinition> = {
+  additionalProperties: false,
+  properties: {
+    name: {
+      type: "string",
+    },
+    region: {
+      type: "string",
     },
   },
-});
+  required: ["name", "region"],
+  type: "object",
+};
+
+const CLOUDFRONT_DEFINITION_SCHEMA: JSONSchemaType<CloudfrontDefinition> = {
+  additionalProperties: false,
+  properties: {
+    id: {
+      type: "string",
+    },
+    region: {
+      type: "string",
+    },
+  },
+  required: ["id", "region"],
+  type: "object",
+};
+
+const SCHEMA: JSONSchemaType<Config> = {
+  additionalProperties: false,
+  properties: {
+    assets: {
+      additionalProperties: {
+        anyOf: [
+          {
+            type: "boolean",
+          },
+          ASSET_DEFINITION_SCHEMA,
+        ],
+      },
+      nullable: true,
+      type: "object",
+    },
+    bucket: BUCKET_DEFINITION_SCHEMA,
+    buildDir: {
+      type: "string",
+    },
+    cloudfront: { ...CLOUDFRONT_DEFINITION_SCHEMA, nullable: true },
+    confirmation: {
+      items: {
+        type: "string",
+      },
+      nullable: true,
+      type: "array",
+    },
+  },
+  required: ["bucket", "buildDir"],
+  type: "object",
+};
 
 export function loadConfig(): Config | null {
   const loader = cosmiconfigSync(COSMICONFIG_MODULE_NAME);
@@ -156,6 +88,15 @@ export function loadConfig(): Config | null {
     return null;
   }
 
-  const validated = CONFIG_VALIDATOR.load<Config>(configFile.config).validate();
-  return validated.getProperties();
+  const config: unknown = configFile.config;
+
+  const ajv = new Ajv();
+  const validate = ajv.compile(SCHEMA);
+  const valid = validate(config);
+  if (!valid) {
+    console.error(validate.errors);
+    return null;
+  }
+
+  return config;
 }
